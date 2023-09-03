7f432a196c6e147c5e16e7ef91469f826222f4d5
Jean Eckelberg <fyrelynx11@gmail.com>
2023-09-02 22:48:53 -0500

    Added a lot of new prompts

--- utils.py
+++ utils.py
@@ -4,15 +4,32 @@ CiviPrompts = [
     'Pick a happy song!',
     'Pick a sad song!',
     'Pick a funny song!',
+    'Pick a hype song!',
+    'Pick a rock song!',
 ]
 
 TraitorPrompts = [
     ('1', 'Pick a song that make you feel like dancing!'),
     ('1', 'Pick a song with a funky beat!'),
+    ('1', 'Pick a song you used to like a lot!'),
+    ('1', 'Pick a song you like that you think the person being tested with you would like!'),
     ('2', 'Pick a song to play at a funeral!'),
     ('2', 'Pick a song to play when you\'re stressed out!'),
+    ('2', 'Pick a song for a nighttime drive!'),
+    ('2', 'Pick an emotional song!'),
+    ('2', 'Pick a song you like that you think the person being tested with you would like!'),
     ('3', 'Pick a children\'s song!'),
     ('3', 'Pick a song that you would hear in a carnival!'),
+    ('3', 'Pick a song you like that you think the person to your left would like!'),
+    ('4', 'Pick a song that you repeat often!'),
+    ('4', 'Pick a song you like that you think the person to your right would like!'),
+    ('4', 'Pick a song with a meaningful message'),
+    ('4', 'Pick a song that makes mundane tasks feel epic'),
+    ('5', 'Pick a song for drive for a road trip!'),
+    ('5', 'Pick a song you like that you think the person across from you would like!'),
+    ('5', 'Pick a song that makes mundane tasks feel epic'),
+    ('5', 'Pick a song you\'d workout to')
+
 ]
 
 
